22e30bbd7d8308b311cdaa5ddd2858e7c41fab1d
DC <tolotrarandria25@gmail.com>
2026-02-11 10:05:03 +0300

    feat: Implement Create functionnality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Editor from "./components/Editor";
 import NotesList from "./components/NotesList"
 import Sidebar from "./components/Sidebar"
+import { useState } from "react";
 
 function App() {
+    const [notes, setNotes] = useState(() => {
+        return JSON.parse(localStorage.getItem("notes")) || [];
+    });
+
+    function createNote() {
+        const newNote = {
+            id: Date.now(),
+            title: "Nouvelle note",
+            content: "",
+            updatedAt: Date.now(),
+        };
+        setNotes([newNote, ...notes]);
+        console.log(notes);
+    }
 
   return (
     <div className="h-screen grid grid-cols-[240px_320px_1fr] bg-base-200">
-      <Sidebar />
-      <NotesList />
-      <Editor />
+        <Sidebar
+            onCreate={createNote}
+        />
+        <NotesList />
+        <Editor />
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { Notebook, Edit, PinIcon, Folder, Trash2 } from "lucide-react";
 
-export default function Sidebar (){
+export default function Sidebar ({onCreate}){
     return(
         <aside className="bg-base-100 border-r border-base-300 p-4">
             <div className="flex gap-1">
@@ -8,7 +8,7 @@ export default function Sidebar (){
                 <h1 className="text-xl font-bold mb-6">DCNotes</h1>
             </div>
 
-             <button className="btn btn-primary w-full mb-4"><Edit className="w-4 h-4"/> Nouvelle note</button>
+             <button className="btn btn-primary w-full mb-4" onClick={onCreate}><Edit className="w-4 h-4"/> Nouvelle note</button>
 
              <ul className="menu menu-sm gap-1 w-full">
                 <li><a><PinIcon className="w-4 h-4"/> Epinglées</a></li>
